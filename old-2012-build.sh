3bb5812e79b05b6ad1b989d3fc9795f9c2c01768
Embers McKinney <em@embermckinney.com>
2021-03-04 21:19:15 -0500

    Build 2012 Chat

---
+++ old-2012-build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd "$(dirname "$0")/old-2012-highschool-chat"
+find -name '*.class' -exec rm {} +
+javac ./nighttimedriver/chat/testing/HTMLChatUITest.java \
+    $(find . -name '*.java' ! -path './nighttimedriver/chat/testing/*')
+jar cvfm htmlchat.jar \
+    <(echo 'Main-Class: nighttimedriver.chat.testing.HTMLChatUITest') \
+    nighttimedriver
+find -name '*.class' -exec rm {} +
